6eef537a8b47334adcdc56b805530160878fe776
DarleneLL <darlene_lima@estudante.sesisenai.org.br>
2024-05-27 14:43:59 -0300

    função para retornar números pares

---
+++ Modulo2/semana02/src/arrayExercicio1.js
@@ -0,0 +1,8 @@
+//Função que retorne um novo array, contendo apenas os números pares
+
+function retornaNumPares(numeros) {
+    return numeros.filter(numero => numero % 2 === 0);
+}
+
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(retornaNumPares(numeros));
